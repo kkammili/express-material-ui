ed1eab8aaf44a8effd5e66ae469f33392af62792
krishna <kkrajus777@gmail.com>
2019-04-15 16:29:34 -0500

    feat(logger): logger now shows javascript instead of immutable objects

--- redux/store.js
+++ redux/store.js
@@ -4,10 +4,21 @@ import thunk from 'redux-thunk'
 import {routerMiddleware} from 'react-router-redux'
 import {createMemoryHistory} from 'history'
 import reducer from './reducer'
-import logger from 'redux-logger'
+import {createLogger} from 'redux-logger'
 import {composeWithDevTools} from 'redux-devtools-extension'
-
+import Immutable from 'immutable'
 export const history = createMemoryHistory()
+
+
+
+const logger = createLogger({
+    stateTransformer: (state) => {
+        if(Immutable.isImmutable(state)) return state.toJS()
+        else return state;
+    } ,
+    collapsed:true
+});
+
 const middlewares = [
   thunk,
   routerMiddleware(history),
